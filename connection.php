75619843f4367c8c5f2c8468a2faae2b11acc77f
Faride Zaker <farideh.zaker8217@yahoo.com>
2022-07-06 12:59:53 +0430

    my start

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+try{
+    $server = "localhost";
+    $database = "snowdb";
+    $DBusername = "root";
+    $DBpassword = "";
+    $dsn = "mysql:host=$server;dbname=$database;charset=utf8";
+    
+    $connect = new PDO($dsn,$DBusername,$DBpassword);
+}
+catch(PDOExeption $error)
+{
+    echo "there is an erorr is connecting";
+}
+?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-rtl.css">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div class="container-fluid">
+    <a class="navbar-brand" href="#">برف</a>
+</div>  
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">خانه</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">نمونه کار</a>
+    </li>
+    </ul>
+</div>
+  
+</nav>
+
+<script src="assets/js/bootstrap.js"></script>
+</body>
+</html>
