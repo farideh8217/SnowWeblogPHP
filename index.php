f7e8488997ee0994b69de45487672100ccbd8d78
Faride Zaker <farideh.zaker8217@yahoo.com>
2022-07-06 22:40:40 +0430

    navbar & slider

--- index.php
+++ index.php
@@ -6,25 +6,59 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/bootstrap.css">
     <link rel="stylesheet" href="assets/css/bootstrap-rtl.css">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
+<div class="top-side">
+    <div class="container">
+        <h6>امروز 1401/04/15می باشد</h6>
+    </div>    
+</div>    
+<div class="main-nav">
+    <img src="assets/images/1.png" >
+</div> 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-<div class="container-fluid">
-    <a class="navbar-brand" href="#">برف</a>
-</div>  
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-    <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">خانه</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">نمونه کار</a>
-    </li>
-    </ul>
-</div>
-  
+    <div class="asli">
+        <a class="navbar-brand" href="#">برف</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">خانه</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">نمونه کار</a>
+            </li>
+            </ul>
+        </div>
+    </div>  
 </nav>
+<div class="container">
+    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="10000">
+        <img src="assets/images/1.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
+        <img src="assets/images/1.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="assets/images/1.png" class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+</div>    
 
 <script src="assets/js/bootstrap.js"></script>
 </body>
